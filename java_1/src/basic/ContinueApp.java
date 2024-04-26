c2e83a536295663402bb1e727f915729a94859e7
rnjst <rnjst@192.168.219.107>
2024-04-27 01:34:20 +0900

    240425

--- java_1/src/basic/ContinueApp.java
+++ java_1/src/basic/ContinueApp.java
@@ -3,10 +3,11 @@ package basic;
 //continue : 반복문의 명령을 처음부터 다시 실행하기 위한 키워드
 // => 반복문의 블록 내부에서 continue 명령이 실행되면 하단에 작성된 명령을 실행하지 않고
 //블럭 내부의 명령을 처음부터 다시 실행
+
 public class ContinueApp {
 	public static void main(String[] args) {
 		for(int i=1;i<=5;i++) {
-			if(i == 3) continue;//3일때만 건너뜀
+			if(i == 3) continue;
 			System.out.println("i = "+i);
 		}
 		System.out.println("==============================================================");
@@ -19,10 +20,10 @@ public class ContinueApp {
 		}
 		System.out.println("==============================================================");
 		loop:
-		for(int i=1;i<=5;i++) {//이곳으로 돌아옴
+		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=5;j++) {
 				//continue 라벨명 : 라벨명으로 지정된 반복문을 다시 실행
-				if(j == 3) continue loop;//for(int i=1;i<=5;i++) 재실행//break와 같은결과
+				if(j == 3) continue loop;//for(int i=1;i<=5;i++) 재실행
 				System.out.println("i = "+i+", j = "+j);
 			}
 		}
--- java_1/src/basic/IfApp.java
+++ java_1/src/basic/IfApp.java
@@ -3,19 +3,19 @@ package basic;
 //제어문(Control Statement) : 프로그램의 흐름(스레드 - Thread)을 바꾸는 명령
 // => 선택문(if, switch), 반복문(for, while), 기타(break, continue, return)
 
-//if : 조건식에 의해 명령을 선택 실행하기 위한 명령을 작성하기 위한 키워드
+//if : 조건식에 의해 명령을 선택 실행하기 위한 문장을 작성하는 키워드
 
-//형식-1) if(조건식) { 명령; 명령; ...}
+//형식-1) if(조건식) { 명령; 명령; ... }
 // => 조건식의 결과값이 [true]인 경우 블럭 내부에 작성된 명령들을 실행
 // => 블럭 내부에 작성된 명령이 하나인 경우 블럭 기호 생략 가능
 
-//형식-2) if(조건식) { 명령; 명령; ...} else { 명령; 명령; ...}
+//형식-2) if(조건식) { 명령; 명령; ... } else { 명령; 명령; ... }
 // => 조건식의 결과값을 구분하여 명령을 선택 실행
 
-//형식-3) if(조건식) { 명령; 명령; ...} else if(조건식) { 명령; 명령; ...}
-//			             else if(조건식) { 명령; 명령; ...} [else { 명령; 명령; ..}]
-//=> 다수의 조건식에 작성하여 조건식이 참인 블럭 내부의 명령을 선택 실행
-//=> 모든 조건식이 거짓인 경우 else 블럭의 명령 실행 - else 생략 가능
+//형식-3) if(조건식) { 명령; 명령; ... } else if(조건식) { 명령; 명령; ... }
+// 			else if(조건식) { 명령; 명령; ... } [else { 명령; 명령; ... }]
+// => 다수의 조건식에 작성하여 조건식이 참인 블럭 내부의 명령을 선택 실행
+// => 모든 조건식이 거짓인 경우 else 블럭의 명령 실행 - else 생략 가능
 
 public class IfApp {
 	public static void main(String[] args) {
@@ -24,34 +24,34 @@ public class IfApp {
 		
 		//변수값이 50 이상인 경우 변수값을 화면에 출력하는 프로그램 작성
 		/*
-		if(su >=50) {
+		if(su >= 50) {
 			System.out.println("su = "+su);
 		}
 		*/
 		
 		//조건식이 참인 경우 실행될 명령이 하나인 경우 블럭 기호 생략 가능
-		if(su >=50) System.out.println("su = "+su);
-		System.out.println("=======================================================");
+		if(su >= 50) System.out.println("su = "+su);
+		System.out.println("==============================================================");
 		//int score=80;
 		int score=50;
 		
 		//변수값이 60 이상인 경우 합격 메세지를 출력하고 60 미만인 경우 불합격 메세지를 출력하는 프로그램 작성
 		/*
 		if(score >= 60) {
-			System.out.println("[결과]점수가 60점 이상이므로 <합격>입니다.");			
+			System.out.println("[결과]점수가 60점 이상이므로 <합격>입니다.");
 		}
-		
+
 		if(score < 60) {
-			System.out.println("[결과]점수가 60점 미만이므로 <불합격>입니다.");			
+			System.out.println("[결과]점수가 60점 미만이므로 <불합격>입니다.");
 		}
 		*/
 		
 		if(score >= 60) {
-			System.out.println("[결과]점수가 60점 이상이므로 <합격>입니다.");			
-		} else {		
-			System.out.println("[결과]점수가 60점 미만이므로 <불합격>입니다.");			
+			System.out.println("[결과]점수가 60점 이상이므로 <합격>입니다.");
+		} else {
+			System.out.println("[결과]점수가 60점 미만이므로 <불합격>입니다.");
 		}
-		System.out.println("=======================================================");
+		System.out.println("==============================================================");
 		int num=10;
 		
 		//변수값을 홀수와 짝수로 구분하여 출력하는 프로그램 작성
@@ -61,7 +61,7 @@ public class IfApp {
 		}
 		
 		if(num % 2 == 0) {
-			System.out.println(num+" = 짝수");				
+			System.out.println(num+" = 짝수");
 		}
 		*/
 		
@@ -70,22 +70,22 @@ public class IfApp {
 		} else {
 			System.out.println(num+" = 짝수");
 		}
-		System.out.println("=======================================================");
+		System.out.println("==============================================================");
 		//char mun='O';
 		char mun='0';
 		
 		//변수값을 영문자 또는 비영문자로 구분하여 출력하는 프로그램 작성
-		if(mun >= 'A' && mun <='Z' || mun >= 'a' && mun <='z') {
+		if(mun >= 'A' && mun <= 'Z' || mun >= 'a' && mun <= 'z') {
 			System.out.println("[결과]"+mun+" = 영문자");
 		} else {
 			System.out.println("[결과]"+mun+" = 비영문자");
 		}
-		System.out.println("=======================================================");
+		System.out.println("==============================================================");
 		//boolean bool=true;
 		boolean bool=false;
 		
 		//조건식 대신 논리값을 저장한 변수를 사용하여 명령 선택 실행
-		/*		
+		/*
 		if(bool) {
 			System.out.println("현재 변수값은 [true]입니다.");
 		} else {
@@ -98,7 +98,7 @@ public class IfApp {
 		} else {
 			System.out.println("현재 변수값은 [true]입니다.");
 		}
-		System.out.println("=======================================================");
+		System.out.println("==============================================================");
 		int jumsu=85;
 		//int jumsu=185;
 		
@@ -110,7 +110,7 @@ public class IfApp {
 			//변수값(점수)을 이용해 등급(학점)을 구분하여 출력 처리
 			// => 100 ~ 90 : A, 89 ~ 80 : B, 79 ~ 70 : C, 69 ~ 60 : D, 59 ~ 0 : F
 			String grade="";//학점을 저장하기 위한 변수
-			
+
 			/*
 			if(jumsu <= 100 && jumsu >= 90) {
 				grade="A";
@@ -144,9 +144,8 @@ public class IfApp {
 			
 			System.out.println("[결과]"+jumsu+"점 = "+grade+"학점");
 		} else {
-			System.out.println("[에러]0~100 범위를 벗어난 비정상적인 점수가 입력 되었습니다. ");
+			System.out.println("[에러]0~100 범위를 벗어난 비정상적인 점수가 입력 되었습니다.");
 		}
-		System.out.println("=======================================================");
-			
+		System.out.println("==============================================================");
 	}
 }
