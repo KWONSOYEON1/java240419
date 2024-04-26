5e5954524ca7ca10298cabec4acd010e500c79da
itwill <itwill@DESKTOP-I0JRR21>
2024-04-26 18:27:38 +0900

    240426

--- java_1/src/example/ForExample.java
+++ java_1/src/example/ForExample.java
@@ -6,18 +6,24 @@ public class ForExample {
 		//ex) 홍길동 홍길동 홍길동 홍길동 홍길동 홍길동 홍길동
 		for(int i=1;i<=7;i++) {
 			System.out.print("권소연 ");
-			}
+		}
 		System.out.println();
 		
 		System.out.println("=========================================");
 		//1~10 범위의 정수 중 홀수를 화면에 출력하세요.
 		//ex) 1     3     5     7     9
 		for(int i=1;i<=10;i++) {
-			if(i%2!=0 ) {
+			if(i%2!=0) {
 				System.out.print(i+"\t");
 			}
 		}
 		System.out.println();			
+		/*
+		for(int i=1;i<=9;i+=2) {
+			System.out.print(i+"\t");
+		}
+		System.out.println();
+		*/
 		
 		System.out.println("=========================================");
 		//1~100 범위의 정수에서 3의 배수들의 합계와 5의 배수들의 합계를 계산하여
@@ -34,6 +40,14 @@ public class ForExample {
 				fiv+=j;
 			}	
 		System.out.println(thr-fiv);
+		/*
+		int tot3=0, tot5=0;
+		for(int i=1;i<=100;i++) {
+			if(i % 3 == 0) tot3+=i;
+			if(i % 5 == 0) tot5+=i;
+		}
+		System.out.println("결과 = "+(tot3-tot5));
+		*/
 		
 		System.out.println("=========================================");
 		//구구단 중 7단을 화면에 출력하세요.
@@ -44,18 +58,25 @@ public class ForExample {
 		//    7 * 9 = 63
 		for(int i=7;i<=7;i++) {
 			for(int j=1;j<=9;j++) {
-				System.out.println(i+" * "+j+" = "+i*j);
+				System.out.println(i+" * "+j+" = "+(i*j));
 			}
 		}
 		
 		System.out.println("=========================================");
 		//5!의 결과값을 출력하세요.(5! = 5 * 4 * 3 * 2 * 1)
 		//ex) 5! = 120
-		int mul=1;
+		int mul=1; 
 		for(int i=1;i<=5;i++) {
 			mul*=i;
 		}
-		System.out.println("5! = "+mul);		
+		System.out.println("5! = "+mul);
+		/*
+		int fac=1, num=5;
+		for(int i=num;i>=1;i--) {
+			fac*=i;
+		}
+		System.out.println(num+"! = "+fac);
+		*/
 		
 		System.out.println("=========================================");
 		//두 변수에 저장된 범위의 정수값을 화면에 출력하세요.
@@ -65,14 +86,25 @@ public class ForExample {
 		//    50    51    52     53     54     55     56
 		//    57
 		int begin=36, end=57;
-		for(int i=begin;i<=end;i++) {
+			for(int i=begin;i<=end;i++) {
 			System.out.print(i+"\t");
 			if(i%7==0) {
 				System.out.println();
 			}			
 		}
 		System.out.println();
+		/*
+		int begin=36, end=57;
+		int cnt=0;//출력된 정수값의 갯수를 저장하기 위한 변수
 		
+		for(int i=begin;i<=end;i++) {
+			System.out.print(i+"\t");
+			cnt++;
+			if(cnt % 7 == 0) {
+				System.out.println();
+			}
+		}
+		*/
 		System.out.println("=========================================");	
 	}
 }
--- java_1/src/example/MultiforExample.java
+++ java_1/src/example/MultiforExample.java
@@ -8,15 +8,30 @@ public class MultiforExample {
 		//☆☆☆★★
 		//☆☆☆☆★
 		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=(i-1)*1;j++) {
+			for(int j=1;j<=i-1;j++) {
 				System.out.print("☆");
 			}
-			for(int j=5;j>=i*1;j--) {
+			for(int j=5;j>=i;j--) {
 				System.out.print("★");
 			}
 			System.out.println();
 		}
-		
+		/*
+		//★★★★★  i=1  흰별=0  검은별=5   
+		//☆★★★★  i=2  흰별=1  검은별=4
+		//☆☆★★★  i=3  흰별=2  검은별=3
+		//☆☆☆★★  i=4  흰별=3  검은별=2
+		//☆☆☆☆★  i=5  흰별=4  검은별=1
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i-1;j++) {
+				System.out.print("☆");
+			}
+			for(int j=1;j<=6-i;j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		*/
 		System.out.println("===============================================================");
 		//★★★★★
 		//☆★★★★
@@ -27,12 +42,29 @@ public class MultiforExample {
 			for(int j=5;j>=(i+1)*1;j--) {
 				System.out.print("☆");
 			}
-			for(int j=1;j<=i*1;j++) {
+			for(int j=1;j<=i;j++) {
 				System.out.print("★");
 			}
 			System.out.println();
 		}
-		
+		/*
+		// 1 2 3 4 5 >> j
+		//★★★★★  i=1 
+		//☆★★★★  i=2
+		//☆☆★★★  i=3
+		//☆☆☆★★  i=4
+		//☆☆☆☆★  i=5
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i <= j) {
+					System.out.print("★");
+				} else {
+					System.out.print("☆");
+				}
+			}
+			System.out.println();
+		}
+		*/
 		System.out.println("===============================================================");
 		//☆☆☆☆★
 		//☆☆☆★★★
@@ -52,7 +84,22 @@ public class MultiforExample {
 			
 			System.out.println();
 		}
-		
+		/*
+		//☆☆☆☆★          i=1  흰별=4  검은별=1       
+		//☆☆☆★★★        i=2  흰별=3  검은별=3
+		//☆☆★★★★★      i=3  흰별=2  검은별=5
+		//☆★★★★★★★    i=4  흰별=1  검은별=7
+		//★★★★★★★★★  i=5  흰별=0  검은별=9
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5-i;j++) {
+				System.out.print("☆");
+			}
+			for(int j=1;j<=i*2-1;j++) {
+				System.out.print("★");
+			}
+			System.out.println();
+		}
+		*/
 		System.out.println("===============================================================");
 		//★☆☆☆★
 		//☆★☆★☆
