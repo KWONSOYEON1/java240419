dcedee1751dd5e4cd8d635b3c4d5501116be3597
rnjst <rnjst@192.168.219.108>
2024-04-29 04:17:42 +0900

    240426

--- java_1/src/example/UpAndDownExample.java
+++ java_1/src/example/UpAndDownExample.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Scanner;
+
 //컴퓨터로부터 제공받은 정수 난수값을 키보드로 입력하여 맞추는 프로그램을 작성하세요.
 // => 1~100 범위의 정수 난수값을 제공받도록 작성
 // => 난수값을 맞출 수 있는 기회는 10번만 제공되도록 작성
@@ -9,6 +11,21 @@ package example;
 // => 난수값을 10번 안에 맞추지 못한 경우 난수값이 출력되도록 작성
 public class UpAndDownExample {
 	public static void main(String[] args) {
-
+		Scanner scanner=new Scanner(System.in);
+			int count=0;
+		
+			int computer=(int)(Math.random()*100+1);
+			System.out.println("1~100 범위 기회는 10번 입니다.");
+			int user;
+			
+			while (true) {
+				System.out.print("입력 = ");
+				user=scanner.nextInt();
+				if(user<1 || user>100) 
+				System.out.println("[에러] 재입력 하세요.");
+				continue;
+			}
+			
+			
 	}
 }
