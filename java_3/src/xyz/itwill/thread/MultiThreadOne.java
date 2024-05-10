563a825c28e02595e094d486955379adcf275e78
itwill <itwill@DESKTOP-I0JRR21>
2024-05-10 11:49:46 +0900

    240509

--- java_3/src/xyz/itwill/thread/MultiThreadOne.java
+++ java_3/src/xyz/itwill/thread/MultiThreadOne.java
@@ -2,7 +2,7 @@ package xyz.itwill.thread;
 
 //스레드를 생성하기 위해 선언된 클래스
 public class MultiThreadOne extends Thread {
-	@Override
+	@Override//오버라이딩 하면 try~catch만 가능,throws 불가능
 	public void run() {
 		for(char i='A';i<='Z';i++) {
 			System.out.print(i);
