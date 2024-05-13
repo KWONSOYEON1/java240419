63ecfe2aeaeda6d397b4826986914322e4fc3964
rnjst <rnjst@192.168.219.109>
2024-05-14 02:26:21 +0900

    240513

--- java_3/src/xyz/itwill/util/DayCalculateApp.java
+++ java_3/src/xyz/itwill/util/DayCalculateApp.java
@@ -1,11 +1,34 @@
 package xyz.itwill.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 //키보드로 생년월일을 입력받아 오늘까지 살아온 날짜(일)을 계산하여 출력하는 프로그램 작성
 //ex) 생년월일 입력[ex. 2000-01-01] >> 2024-05-12
 //    [결과]태어난지 <1일>이 지났습니다.
 // => 형식에 맞지 않는 생년월일을 입력한 경우 에러 메세지 출력 후 프로그램 종료
 public class DayCalculateApp {
 	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("생년월일 입력[ex. 2000-01-01] >> ");
+		String birthday=scanner.nextLine();
 		
+		SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd");
+		
+		try {		
+			Date bir = dateFormat.parse(birthday);
+			Date now = new Date();
+		         
+			long difference = now.getTime() - bir.getTime();
+			
+            System.out.println("[결과]태어난지 <" +difference/(1000 * 86400)+ "일>이 지났습니다.");
+        } catch (ParseException e) {
+            System.out.println("[에러]형식에 맞는 생년월일을 입력해 주세요.");
+        }
+		
+		
+		scanner.close();
 	}
 }
