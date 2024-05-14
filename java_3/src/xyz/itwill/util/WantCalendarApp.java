11dccd2b90e0b9bbf6dd7d0f99137e8799449ce5
itwill <itwill@DESKTOP-I0JRR21>
2024-05-14 12:27:20 +0900

    240514

--- java_3/src/xyz/itwill/util/WantCalendarApp.java
+++ java_3/src/xyz/itwill/util/WantCalendarApp.java
@@ -8,15 +8,20 @@ public class WantCalendarApp {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		
-		System.out.print("년: ");
-		int year=Integer.parseInt(scanner.nextLine());
-		System.out.print("월: ");
-		int month=Integer.parseInt(scanner.nextLine());
+		System.out.print("년 >> ");
+		int year=scanner.nextInt();
+		
+		System.out.print("월 >> ");
+		int month=scanner.nextInt();
 	
 		scanner.close();
 		
+		//플렛폼의 현재 날짜와 시간이 저장된 Calendar 객체를 반환받아 저장
 		Calendar calendar=Calendar.getInstance();
 		
+		//Calendar 객체에 저장된 날짜와 시간 중 [년월일]을 키보드로 입력받은 [년]과 [월]의
+		//[1]일로 변경 처리
+		// => Calendar 객체는 [월]을 0~11 범위의 정수값으로 표현하므로 키보드 입력값을 [1] 감소하여 변경
 		calendar.set(year, month-1, 1);
 		
 		int week=calendar.get(Calendar.DAY_OF_WEEK);				
@@ -29,17 +34,19 @@ public class WantCalendarApp {
 		for(int i=1;i<week;i++) {
 			System.out.print("   ");
 		}
+		
 		for(int i=1;i<=calendar.getActualMaximum(Calendar.DATE);i++) {
 			if(i<=9) {
 				System.out.print("  "+i);
 			} else {
 				System.out.print(" "+i);
-				}
+			}
+			
 			week++;
 			
-		if(week % 7 == 1) {
-			System.out.println("");
-		}
+			if(week % 7 == 1) {
+				System.out.println();
+			}
 			
 		}
 		System.out.println();
