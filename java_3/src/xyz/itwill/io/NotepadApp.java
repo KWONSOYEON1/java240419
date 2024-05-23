38a370c686bc9ce49ed2155522ba1fd177e00623
itwill <itwill@DESKTOP-I0JRR21>
2024-05-23 10:55:56 +0900

    240523

--- java_3/src/xyz/itwill/io/NotepadApp.java
+++ java_3/src/xyz/itwill/io/NotepadApp.java
@@ -124,7 +124,7 @@ public class NotepadApp extends JFrame {
 					
 					try {
 						BufferedReader in=new BufferedReader(new FileReader(file.getAbsoluteFile()));
-						
+						 
 						textArea.setText("");//JTextArea 컴퍼넌트 초기화 처리
 						
 						while(true) {
@@ -149,27 +149,51 @@ public class NotepadApp extends JFrame {
 					return;
 				}
 			} else if(eventSource == save) {
+				if(file == null) {//현재 작업중인 파일이 없는 경우
+					//JFleChooser.showSaveDialog(Component parent) : 저장 관련 파일 다이얼로그를
+					//화면에 출력하는 메소드 - [저장] 또는 [취소] 버튼 선택에 따른 정수값 반환
 					int option=fileChooser.showSaveDialog(NotepadApp.this);
-				
-				if(option == JFileChooser.APPROVE_OPTION) {
-					file=fileChooser.getSelectedFile();
 					
-					try (BufferedWriter out=new BufferedWriter(new FileWriter(file))) {
-					        out.write(textArea.getText()); 
-					        
-					        JOptionPane.showMessageDialog(NotepadApp.this, "파일이 성공적으로 저장되었습니다.");
+					if(option == JFileChooser.APPROVE_OPTION) {//파일 선택 후 [저장] 버튼을 누른 경우
+						file=fileChooser.getSelectedFile();
+						 
+						//선택한 파일에 확장자가 없는 경우 [txt] 확장자를 붙여 File 객체 생성
+						if(file.toString().lastIndexOf(".") == -1) {
+							file=new File(file.toString()+".txt");
+						}
+						
+						setTitle(file.toString()+" - Java 메모장");
+						
+						try {
+							BufferedWriter out=new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+							
+							//JTextArea 컴퍼넌트에 존재하는 모든 문자열을 반환받아 저장
+							String text=textArea.getText();
+							
+							out.write(text);
+							
+							out.close();
+						} catch (IOException exception) {
+							JOptionPane.showMessageDialog(NotepadApp.this, "프로그램에 문제가 발생 하였습니다.");
+						}
+					} else if(option == JFileChooser.CANCEL_OPTION) {//[취소] 버튼을 누른 경우
+						return;
+					}
+				} else {//현재 작업중인 파일이 있는 경우
+					try {
+						BufferedWriter out=new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+						String text=textArea.getText();
+						out.write(text);
+						out.close();
 					} catch (IOException exception) {
-					        JOptionPane.showMessageDialog(NotepadApp.this, "파일을 찾을 수 없습니다.");
-					       
-					    }
-				} else if (option == JFileChooser.CANCEL_OPTION) {
-					   return;
+						JOptionPane.showMessageDialog(NotepadApp.this, "프로그램에 문제가 발생 하였습니다.");
+					}
 				}
-									
+
 				
 			} else if(eventSource == exit) {
 				System.exit(0);
-			}					
+			}
 		}
 	}
 }
