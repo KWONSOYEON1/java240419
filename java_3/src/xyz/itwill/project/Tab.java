399562c8e7c9b9a012b80b1e8b4bec26d0c7f404
itwill <itwill@DESKTOP-I0JRR21>
2024-06-07 18:12:36 +0900

    240607

--- java_3/src/xyz/itwill/project/Tab.java
+++ java_3/src/xyz/itwill/project/Tab.java
@@ -8,6 +8,14 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Tab extends JFrame {
 
@@ -16,6 +24,7 @@ public class Tab extends JFrame {
 	private JTable table;
 	private JTable table_1;
 	private JTable table_2;
+	private JTable table_3;
 
 	/**
 	 * Launch the application.
@@ -38,7 +47,7 @@ public class Tab extends JFrame {
 	 */
 	public Tab() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 341);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -48,6 +57,12 @@ public class Tab extends JFrame {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(0, 0, 434, 251);
 		contentPane.add(tabbedPane);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		tabbedPane.addTab("시술 관리", null, scrollPane_2, null);
+		
+		table_2 = new JTable();
+		scrollPane_2.setViewportView(table_2);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		tabbedPane.addTab("회원 관리", null, scrollPane, null);
@@ -61,10 +76,59 @@ public class Tab extends JFrame {
 		table_1 = new JTable();
 		scrollPane_1.setViewportView(table_1);
 		
-		JScrollPane scrollPane_2 = new JScrollPane();
-		tabbedPane.addTab("시술 관리", null, scrollPane_2, null);
+		JScrollPane scrollPane_3 = new JScrollPane();
+		tabbedPane.addTab("예약 관리", null, scrollPane_3, null);
 		
-		table_2 = new JTable();
-		scrollPane_2.setViewportView(table_2);
+		table_3 = new JTable();
+		scrollPane_3.setViewportView(table_3);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 250, 434, 42);
+		contentPane.add(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{95, 57, 57, 57, 57, 57, 0};
+		gbl_panel.rowHeights = new int[]{23, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton btnNewButton_2 = new JButton("삭제");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton_1 = new JButton("등록");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnNewButton = new JButton("검색");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 0;
+		panel.add(btnNewButton, gbc_btnNewButton);
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_1.gridx = 3;
+		gbc_btnNewButton_1.gridy = 0;
+		panel.add(btnNewButton_1, gbc_btnNewButton_1);
+		
+		JButton btnNewButton_3 = new JButton("수정");
+		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
+		gbc_btnNewButton_3.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_3.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton_3.gridx = 4;
+		gbc_btnNewButton_3.gridy = 0;
+		panel.add(btnNewButton_3, gbc_btnNewButton_3);
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnNewButton_2.gridx = 5;
+		gbc_btnNewButton_2.gridy = 0;
+		panel.add(btnNewButton_2, gbc_btnNewButton_2);
 	}
 }
