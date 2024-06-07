ac301949243dc24180c0bf3a5b8532a1b491f2fe
itwill <itwill@DESKTOP-I0JRR21>
2024-06-07 17:56:47 +0900

    240607

---
+++ jdbc/src/xyz/itwill/jdbc/Tab.java
@@ -0,0 +1,70 @@
+package xyz.itwill.jdbc;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+
+public class Tab extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+	private JTable table_1;
+	private JTable table_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tab frame = new Tab();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tab() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(0, 0, 434, 251);
+		contentPane.add(tabbedPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		tabbedPane.addTab("회원 관리", null, scrollPane, null);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		tabbedPane.addTab("직원 관리", null, scrollPane_1, null);
+		
+		table_1 = new JTable();
+		scrollPane_1.setViewportView(table_1);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		tabbedPane.addTab("시술 관리", null, scrollPane_2, null);
+		
+		table_2 = new JTable();
+		scrollPane_2.setViewportView(table_2);
+	}
+}
