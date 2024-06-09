c095d72c37e3bcad102d7556f65b8c17bb393efb
rnjst <rnjst@192.168.219.109>
2024-06-09 22:12:49 +0900

    240609

--- project/src/xyz/itwill/project/MainLogin.java
+++ project/src/xyz/itwill/project/MainLogin.java
@@ -5,11 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.JTextField;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 
@@ -19,6 +23,9 @@ public class MainLogin extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JCheckBox chckbxNonMember;
+	private JCheckBox chckbxMember;
+	private JCheckBox chckbxAdmin;
 
 	/**
 	 * Launch the application.
@@ -90,41 +97,103 @@ public class MainLogin extends JFrame {
 		contentPane.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 		
-		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("비회원");
-		GridBagConstraints gbc_chckbxNewCheckBox_1 = new GridBagConstraints();
-		gbc_chckbxNewCheckBox_1.insets = new Insets(0, 0, 5, 5);
-		gbc_chckbxNewCheckBox_1.gridx = 2;
-		gbc_chckbxNewCheckBox_1.gridy = 3;
-		contentPane.add(chckbxNewCheckBox_1, gbc_chckbxNewCheckBox_1);
+		chckbxNonMember = new JCheckBox("비회원");
+		GridBagConstraints gbc_chckbxNonMember = new GridBagConstraints();
+		gbc_chckbxNonMember.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxNonMember.gridx = 2;
+		gbc_chckbxNonMember.gridy = 3;
+		contentPane.add(chckbxNonMember, gbc_chckbxNonMember);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("회원");
-		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
-		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 5, 5);
-		gbc_chckbxNewCheckBox.gridx = 3;
-		gbc_chckbxNewCheckBox.gridy = 3;
-		contentPane.add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
+		chckbxMember = new JCheckBox("회원");
+		GridBagConstraints gbc_chckbxMember = new GridBagConstraints();
+		gbc_chckbxMember.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxMember.gridx = 3;
+		gbc_chckbxMember.gridy = 3;
+		contentPane.add(chckbxMember, gbc_chckbxMember);
 		
-		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("관리자");
-		GridBagConstraints gbc_chckbxNewCheckBox_2 = new GridBagConstraints();
-		gbc_chckbxNewCheckBox_2.gridwidth = 2;
-		gbc_chckbxNewCheckBox_2.insets = new Insets(0, 0, 5, 0);
-		gbc_chckbxNewCheckBox_2.gridx = 4;
-		gbc_chckbxNewCheckBox_2.gridy = 3;
-		contentPane.add(chckbxNewCheckBox_2, gbc_chckbxNewCheckBox_2);
-		
+		chckbxAdmin = new JCheckBox("관리자");
+		GridBagConstraints gbc_chckbxAdmin = new GridBagConstraints();
+		gbc_chckbxAdmin.gridwidth = 2;
+		gbc_chckbxAdmin.insets = new Insets(0, 0, 5, 0);
+		gbc_chckbxAdmin.gridx = 4;
+		gbc_chckbxAdmin.gridy = 3;
+		contentPane.add(chckbxAdmin, gbc_chckbxAdmin);
+				
 		JButton btnNewButton = new JButton("로그인");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(chckbxNonMember.isSelected()) {					
+				} else if(chckbxMember.isSelected()) {					
+				} else if(chckbxAdmin.isSelected()) {
+					
+				}
+			}
+		});
+		
+		 chckbxNonMember.addActionListener(new ActionListener() {
+		        public void actionPerformed(ActionEvent e) {
+		            if (chckbxNonMember.isSelected()) {
+		                chckbxMember.setSelected(false);
+		                chckbxAdmin.setSelected(false);
+		            }
+		        }
+		    });
+
+		    chckbxMember.addActionListener(new ActionListener() {
+		        public void actionPerformed(ActionEvent e) {
+		            if (chckbxMember.isSelected()) {
+		                chckbxNonMember.setSelected(false);
+		                chckbxAdmin.setSelected(false);
+		            }
+		        }
+		    });
+
+		    chckbxAdmin.addActionListener(new ActionListener() {
+		        public void actionPerformed(ActionEvent e) {
+		            if (chckbxAdmin.isSelected()) {
+		                chckbxNonMember.setSelected(false);
+		                chckbxMember.setSelected(false);
+		            }
+		        }
+		    });
+		    
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewButton.gridx = 2;
 		gbc_btnNewButton.gridy = 5;
-		contentPane.add(btnNewButton, gbc_btnNewButton);
+		contentPane.add(btnNewButton, gbc_btnNewButton);		
 		
 		JButton btnNewButton_1 = new JButton("취소");
+		btnNewButton_1.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        dispose(); 
+		    }
+		});
+		
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
 		gbc_btnNewButton_1.insets = new Insets(0, 0, 0, 5);
 		gbc_btnNewButton_1.gridx = 3;
 		gbc_btnNewButton_1.gridy = 5;
 		contentPane.add(btnNewButton_1, gbc_btnNewButton_1);
-	}
+				
+		btnNewButton.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent e) {
+		        if (chckbxNonMember.isSelected()) {		         
+		        	GuestLogin GuestLogin = new GuestLogin();
+		        	GuestLogin.setVisible(true);
+		        	setVisible(false);
+		        } else if (chckbxMember.isSelected()) {		            
+		        	MemberLogin MemberLogin = new MemberLogin();
+		        	MemberLogin.setVisible(true);
+		        	setVisible(false);
+		        } else if (chckbxAdmin.isSelected()) {		           
+		        	AdministratorLogin AdministratorLogin = new AdministratorLogin();
+		        	AdministratorLogin.setVisible(true);
+		        	setVisible(false);
+		        }
+		    }
+		});	
+		
+		}
 
 }
