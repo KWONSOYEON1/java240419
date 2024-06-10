ba6ddc9c6787df5b6eb31cd0c42ffb3a3b33f85f
rnjst <rnjst@192.168.219.109>
2024-06-10 21:24:24 +0900

    240610

--- project/src/xyz/itwill/project/GuestLogin.java
+++ project/src/xyz/itwill/project/GuestLogin.java
@@ -5,8 +5,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.JTabbedPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
