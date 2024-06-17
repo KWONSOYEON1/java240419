4f3d2f1df823aeecd6e4392d9fd761e8e85264d7
rnjst <rnjst@192.168.219.107>
2024-06-17 22:51:34 +0900

    240617

---
+++ project1/src/xyz/itwill/project/MenuInsert.java
@@ -0,0 +1,278 @@
+package xyz.itwill.project;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import xyz.itwill.project.dao.MenuDAO;
+import xyz.itwill.project.dao.MenuDTO;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+
+import javax.swing.JTextField;
+
+public class MenuInsert extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField priceTF;
+	private JTextField valueTF;
+	private JTextField mtimeTF;
+	
+//	JTextField mnoTF,valueTF,mtimeTF,priceTF;
+	/**
+	 * Launch the application.
+	 */
+//	public static void main(String[] args) {
+//		try {
+//			MenuInsert dialog = new MenuInsert();
+//			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+//			dialog.setVisible(true);			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public MenuInsert(AdministratorLogin administratorLogin) {
+		setTitle("시술 등록");
+		setBounds(100, 100, 450, 400);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{100, 60, 150, 100, 0};
+		gbl_contentPanel.rowHeights = new int[]{23, 23, 23, 23, 23, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
+/*
+		{
+			JLabel lblNewLabel = new JLabel("시술 번호");
+			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+			gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+			gbc_lblNewLabel.gridx = 1;
+			gbc_lblNewLabel.gridy = 1;
+			contentPanel.add(lblNewLabel, gbc_lblNewLabel);
+		}
+		{
+			mnoTF = new JTextField();
+			mnoTF.setEnabled(false);
+			GridBagConstraints gbc_mnoTF = new GridBagConstraints();
+			gbc_mnoTF.insets = new Insets(0, 0, 5, 5);
+			gbc_mnoTF.fill = GridBagConstraints.HORIZONTAL;
+			gbc_mnoTF.gridx = 2;
+			gbc_mnoTF.gridy = 1;
+			contentPanel.add(mnoTF, gbc_mnoTF);
+			mnoTF.setColumns(10);
+		}
+*/
+		
+		{
+			JLabel lblNewLabel_1 = new JLabel("시술 종류");
+			GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+			gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+			gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_1.gridx = 1;
+			gbc_lblNewLabel_1.gridy = 2;
+			contentPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		}		
+		{
+			valueTF = new JTextField();
+			GridBagConstraints gbc_valueTF = new GridBagConstraints();
+			gbc_valueTF.insets = new Insets(0, 0, 5, 5);
+			gbc_valueTF.fill = GridBagConstraints.HORIZONTAL;
+			gbc_valueTF.gridx = 2;
+			gbc_valueTF.gridy = 2;
+			contentPanel.add(valueTF, gbc_valueTF);
+			valueTF.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_2 = new JLabel("시술 시간");
+			GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+			gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+			gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+			gbc_lblNewLabel_2.gridx = 1;
+			gbc_lblNewLabel_2.gridy = 3;
+			contentPanel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		}		
+		{
+			mtimeTF = new JTextField();
+			GridBagConstraints gbc_mtimeTF = new GridBagConstraints();
+			gbc_mtimeTF.insets = new Insets(0, 0, 5, 5);
+			gbc_mtimeTF.fill = GridBagConstraints.HORIZONTAL;
+			gbc_mtimeTF.gridx = 2;
+			gbc_mtimeTF.gridy = 3;
+			contentPanel.add(mtimeTF, gbc_mtimeTF);
+			mtimeTF.setColumns(10);
+		}
+		{
+			JLabel lblNewLabel_3 = new JLabel("시술 가격");
+			GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+			gbc_lblNewLabel_3.anchor = GridBagConstraints.WEST;
+			gbc_lblNewLabel_3.insets = new Insets(0, 0, 0, 5);
+			gbc_lblNewLabel_3.gridx = 1;
+			gbc_lblNewLabel_3.gridy = 4;
+			contentPanel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		}
+		{
+			priceTF = new JTextField();
+			GridBagConstraints gbc_priceTF = new GridBagConstraints();
+			gbc_priceTF.insets = new Insets(0, 0, 0, 5);
+			gbc_priceTF.fill = GridBagConstraints.HORIZONTAL;
+			gbc_priceTF.gridx = 2;
+			gbc_priceTF.gridy = 4;
+			contentPanel.add(priceTF, gbc_priceTF);
+			priceTF.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			GridBagLayout gbl_buttonPane = new GridBagLayout();
+			gbl_buttonPane.columnWidths = new int[]{200, 57, 57, 0};
+			gbl_buttonPane.rowHeights = new int[]{23, 23, 23, 23, 23, 23, 23, 0};
+			gbl_buttonPane.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+			gbl_buttonPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+			buttonPane.setLayout(gbl_buttonPane);
+			{
+			}
+			{
+				JButton okButton = new JButton("등록");				
+				okButton.setActionCommand("OK");
+				GridBagConstraints gbc_okButton = new GridBagConstraints();
+				gbc_okButton.anchor = GridBagConstraints.NORTH;
+				gbc_okButton.insets = new Insets(0, 0, 5, 5);
+				gbc_okButton.gridx = 1;
+				gbc_okButton.gridy = 0;
+				buttonPane.add(okButton, gbc_okButton);
+				getRootPane().setDefaultButton(okButton);
+				
+				okButton.addActionListener(e -> {
+					 addMenu();
+					 administratorLogin.displayAllMenu();
+//					 dispose();
+				});
+			}
+			JButton cancelButton = new JButton("취소");
+			cancelButton.setActionCommand("Cancel");
+			GridBagConstraints gbc_cancelButton = new GridBagConstraints();
+			gbc_cancelButton.insets = new Insets(0, 0, 5, 0);
+			gbc_cancelButton.anchor = GridBagConstraints.NORTH;
+			gbc_cancelButton.gridx = 2;
+			gbc_cancelButton.gridy = 0;
+			buttonPane.add(cancelButton, gbc_cancelButton);
+			
+			cancelButton.addActionListener(e -> {
+				dispose(); 
+			});
+		}			
+	}
+	
+	public void addMenu() {		
+//		String mnoString=mnoTF.getText();
+//		
+//		if(mnoString.equals("")) {
+//			JOptionPane.showMessageDialog(this, "시술 번호를 입력해 주세요.");
+//			mnoTF.requestFocus();
+//			return;
+//		}
+//		String mnoReg="^[1-9][0-9]{3}$";
+//		if(!Pattern.matches(mnoReg, mnoString)) {
+//			JOptionPane.showMessageDialog(this, "시술 번호는 4자리 숫자로만 입력해 주세요.");
+//			mnoTF.requestFocus();
+//			return;
+//		}
+	
+//		int mno=Integer.parseInt(mnoString);
+//		
+//		if(MenuDAO.getDAO().selectMenuByMno(mno) != null) {
+//			JOptionPane.showMessageDialog(this, "이미 사용중인 시술 번호를 입력 하였습니다.");
+//			valueTF.requestFocus();
+//			return;
+//		}
+		
+		String value=valueTF.getText();
+		
+		if(value.equals("")) {
+			JOptionPane.showMessageDialog(this, "시술 종류를 입력해 주세요.");
+			valueTF.requestFocus();
+			return;
+		}
+		
+//		String valueReg="^[가-힣]{2,5}$";
+//		if(!Pattern.matches(valueReg, value)) {
+//			JOptionPane.showMessageDialog(this, "시술 종류는 2~5 범위의 한글로만 입력해 주세요.");
+//			valueTF.requestFocus();
+//			return;
+//		}
+			
+		String mtimeString=mtimeTF.getText();
+		
+		if(mtimeString.equals("")) {
+			JOptionPane.showMessageDialog(this, "시술 시간을 입력해 주세요.");
+			mtimeTF.requestFocus();
+			return;
+		}
+		
+//		String mtimeReg="^[1-9][0-9]{3}$";
+//		if(!Pattern.matches(mtimeReg, mtimeString)) {
+//			JOptionPane.showMessageDialog(this, "시술 시간을 형식에 맞게 입력해 주세요.");
+//			mtimeTF.requestFocus();
+//			return;
+//		}
+//		
+		int mtime=Integer.parseInt(mtimeString);
+		
+		String priceString=priceTF.getText();
+		
+		if(priceString.equals("")) {
+			JOptionPane.showMessageDialog(this, "시술 가격을 입력해 주세요.");
+			priceTF.requestFocus();
+			return;
+		}
+		
+//		String priceReg="^[1-9][0-9]{3}$";
+//		if(!Pattern.matches(priceReg, priceString)) {
+//			JOptionPane.showMessageDialog(this, "시술 가격을 형식에 맞게 입력해 주세요.");
+//			priceTF.requestFocus();
+//			return;
+//		}
+		
+		int price=Integer.parseInt(priceString);
+		
+		MenuDTO menu=new MenuDTO();
+		//menu.setMno(mno); // 시퀀스 처리
+		menu.setValue(value);
+		menu.setMtime(mtime);
+		menu.setPrice(price);
+		
+		int rows=MenuDAO.getDAO().insertMenu(menu);
+		
+		if (rows > 0) {
+		JOptionPane.showMessageDialog(this, rows+"개의 시술을 삽입하여 저장 하였습니다.");
+		dispose();
+		// TF 초기화
+		valueTF.setText("");
+		mtimeTF.setText("");
+		priceTF.setText("");
+		
+		} else {
+			JOptionPane.showMessageDialog(this, "입력 양식에 맞는 값을 입력해주세요");
+		}		
+		
+
+		
+		
+	}
+}
---
+++ project1/src/xyz/itwill/project/dao/RsrrvtDAO.java
@@ -0,0 +1,279 @@
+package xyz.itwill.project.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RsrrvtDAO extends JdbcDAO {
+	private static RsrrvtDAO _dao;
+
+	public RsrrvtDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	static {
+		_dao = new RsrrvtDAO();
+	}
+
+	public static RsrrvtDAO get_dao() {
+		return _dao;
+	}
+
+	// 예약 추가
+	public int insertRsrrvt(RsrrvtDTO rsrrvt) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int rows = 0;
+
+		try {
+			con = getConnection();
+
+			String sql = "insert into rsrrvt values(RSRRVT_MENU.NEXTVAL,?,?,?,?,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, rsrrvt.getRdate());
+			pstmt.setInt(2, rsrrvt.getRtime());
+			pstmt.setString(3, rsrrvt.getRdid());
+			pstmt.setString(4, rsrrvt.getRcid());
+			pstmt.setInt(5, rsrrvt.getMenu_no());
+			pstmt.setString(6, rsrrvt.getMemo());
+			pstmt.setInt(7, rsrrvt.getPayment());
+			pstmt.setString(8, rsrrvt.getCash());
+			pstmt.setString(9, rsrrvt.getStatus());
+
+			rows = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]insertRsrrvt() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;
+	}
+
+	// 예약 수정
+	public int updateRsrrvt(RsrrvtDTO rsrrvt) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		int rows = 0;
+
+		try {
+			con = getConnection();
+
+			String sql = "update rsrrvt set rdate=?,rtime=?,rdid=?,rcid=?,menu_no=?,payment=?,cash=?,status=?,memo=?";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, rsrrvt.getRdate());
+			pstmt.setInt(2, rsrrvt.getRtime());
+			pstmt.setString(3, rsrrvt.getRdid());
+			pstmt.setString(4, rsrrvt.getRcid());
+			pstmt.setInt(5, rsrrvt.getMenu_no());
+			pstmt.setInt(6, rsrrvt.getPayment());
+			pstmt.setString(7, rsrrvt.getCash());
+			pstmt.setString(8, rsrrvt.getStatus());
+			pstmt.setString(9, rsrrvt.getMemo());
+
+			rows = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]updateRsrrvt() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;
+	}
+	//삭제가 없길래 제가 만들었는데 확인부탁드려요
+	public int deleteRsrrvt(int rno) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		int rows = 0;
+		try {
+			con = getConnection();
+
+			String sql = "delete from rsrrvt where rno=?";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setInt(1, rno);
+
+			rows = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("[에러]deleteRsrrvt() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt);
+		}
+		return rows;
+	}	
+
+	// 단일 검색 _ rno
+	public RsrrvtDTO selectRsrrvtbyRno(int rno) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		ResultSet rs = null;
+		RsrrvtDTO rsrrvt = null;
+
+		try {
+			con = getConnection();
+
+			String sql = "select rno,rdate,rtime,designer.name dname,member.name cname,menu.value mvalue,payment,cash,status,rsrrvt.memo memo from rsrrvt join member on member.id = rsrrvt.rcid join designer on designer.id = rsrrvt.rdid join menu on menu.mno = rsrrvt.menu_no where rno = ? order by rdate desc, rtime desc";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setInt(1, rno);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				rsrrvt = new RsrrvtDTO();
+
+				rsrrvt.setRno(rs.getInt("rno"));
+				rsrrvt.setRdate(rs.getString("rdate").substring(0, 10));
+				rsrrvt.setRtime(rs.getInt("rtime"));
+				rsrrvt.setDname(rs.getString("dname"));
+				rsrrvt.setCname(rs.getString("cname"));
+				rsrrvt.setMvalue(rs.getString("mvalue"));
+				rsrrvt.setPayment(rs.getInt("payment"));
+				rsrrvt.setCash(rs.getString("cash"));
+				rsrrvt.setStatus(rs.getString("status"));
+				rsrrvt.setMemo(rs.getString("memo"));
+
+			}
+
+		} catch (SQLException e) {
+			System.out.println("[에러]selectRsrrvtbyRno() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return rsrrvt;
+
+	}
+
+	public List<RsrrvtDTO> selectRsrrvtbyRdate(String rdate, String rdname) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		ResultSet rs = null;
+		List<RsrrvtDTO> rsrrvtList = new ArrayList<RsrrvtDTO>();
+
+		try {
+			con = getConnection();
+
+//			String sql = "select rno,rdate,rtime,designer.name dname,member.name cname,menu.value mvalue,payment,cash,status,rsrrvt.memo memo from rsrrvt join member on member.id = rsrrvt.rcid join designer on designer.id = rsrrvt.rdid join menu on menu.mno = rsrrvt.menu_no where rdate = ? order by rdate desc, rtime desc";
+			String sql = "select rno,rdate,rtime,designer.name dname,member.name cname,menu.value mvalue,payment,cash,status,rsrrvt.memo memo from rsrrvt join member on member.id = rsrrvt.rcid join designer on designer.id = rsrrvt.rdid join menu on menu.mno = rsrrvt.menu_no where rdate = ? and designer.name = ? order by rdate desc, rtime desc";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, rdate);
+			pstmt.setString(2, rdname);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				RsrrvtDTO rsrrvt = new RsrrvtDTO();
+
+				rsrrvt.setRno(rs.getInt("rno"));
+				rsrrvt.setRdate(rs.getString("rdate"));
+				rsrrvt.setRtime(rs.getInt("rtime"));
+				rsrrvt.setDname(rs.getString("dname"));
+				rsrrvt.setCname(rs.getString("cname"));
+				rsrrvt.setMvalue(rs.getString("mvalue"));
+				rsrrvt.setPayment(rs.getInt("payment"));
+				rsrrvt.setCash(rs.getString("cash"));
+				rsrrvt.setStatus(rs.getString("status"));
+				rsrrvt.setMemo(rs.getString("memo"));
+
+				rsrrvtList.add(rsrrvt);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("[에러]selectRsrrvtbyRdate() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return rsrrvtList;
+	}
+	public List<RsrrvtDTO> selectRsrrvtbyDname(String cname) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		ResultSet rs = null;
+		List<RsrrvtDTO> rsrrvtList = new ArrayList<RsrrvtDTO>();
+		
+		try {
+			con = getConnection();
+			
+			String sql = "select rno,rdate,rtime,designer.name dname,member.name cname,menu.value mvalue,payment,cash,status,rsrrvt.memo memo from rsrrvt join member on member.id = rsrrvt.rcid join designer on designer.id = rsrrvt.rdid join menu on menu.mno = rsrrvt.menu_no where member.name like '%'||?||'%' order by rdate desc, rtime desc";
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, cname);
+			
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				RsrrvtDTO rsrrvt = new RsrrvtDTO();
+				
+				rsrrvt.setRno(rs.getInt("rno"));
+				rsrrvt.setRdate(rs.getString("rdate"));
+				rsrrvt.setRtime(rs.getInt("rtime"));
+				rsrrvt.setDname(rs.getString("dname"));
+				rsrrvt.setCname(rs.getString("cname"));
+				rsrrvt.setMvalue(rs.getString("mvalue"));
+				rsrrvt.setPayment(rs.getInt("payment"));
+				rsrrvt.setCash(rs.getString("cash"));
+				rsrrvt.setStatus(rs.getString("status"));
+				rsrrvt.setMemo(rs.getString("memo"));
+				
+				rsrrvtList.add(rsrrvt);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("[에러]selectRsrrvtbyRdate() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return rsrrvtList;
+	}
+
+	public List<RsrrvtDTO> selectRsrrvtAll() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		ResultSet rs = null;
+		List<RsrrvtDTO> rsrrvtList = new ArrayList<RsrrvtDTO>();
+
+		try {
+			con = getConnection();
+
+			String sql = "select rno,rdate,rtime,designer.name dname,member.name cname,menu.value mvalue,payment,cash,status,rsrrvt.memo memo from rsrrvt join member on member.id = rsrrvt.rcid join designer on designer.id = rsrrvt.rdid join menu on menu.mno = rsrrvt.menu_no order by rdate desc, rtime desc";
+			pstmt = con.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				RsrrvtDTO rsrrvt = new RsrrvtDTO();
+
+				rsrrvt.setRno(rs.getInt("rno"));
+				rsrrvt.setRdate(rs.getString("rdate").substring(0, 10));
+				rsrrvt.setRtime(rs.getInt("rtime"));
+				rsrrvt.setDname(rs.getString("dname"));
+				rsrrvt.setCname(rs.getString("cname"));
+				rsrrvt.setMvalue(rs.getString("mvalue"));
+				rsrrvt.setPayment(rs.getInt("payment"));
+				rsrrvt.setCash(rs.getString("cash"));
+				rsrrvt.setStatus(rs.getString("status"));
+				rsrrvt.setMemo(rs.getString("memo"));
+
+				rsrrvtList.add(rsrrvt);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("[에러]selectRsrrvtAll() 메소드의 SQL 오류 = " + e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return rsrrvtList;
+	}
+
+
+}
