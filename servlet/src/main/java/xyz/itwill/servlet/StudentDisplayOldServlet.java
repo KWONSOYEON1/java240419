e9719e6a3326b50cb33fdb632555ebb9401d066a
itwill <itwill@DESKTOP-I0JRR21>
2024-07-05 10:38:43 +0900

    240705

--- servlet/src/main/java/xyz/itwill/servlet/StudentDisplayOldServlet.java
+++ servlet/src/main/java/xyz/itwill/servlet/StudentDisplayOldServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//STUDENT 테이블에 저장된 모든 행(학생정보)을 검색하여 HTML 문서에 포함하여 응답하는 서블릿
+//STUDENT 테이블에 저장된 모든 행(학생정보)을 검색해 HTML 문서에 포함하여 응답하는 서블릿
 // => JDBC 기능을 구현하기 위해서는 JDBC 관련 라이브러리 파일(ojdbc 라이브러리)을 프로젝트에 빌드 처리
 @WebServlet("/old.itwill")
 public class StudentDisplayOldServlet extends HttpServlet {
