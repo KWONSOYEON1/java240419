352ee79021ab405fbc4c54103133ff3bfd32a9bb
rnjst <rnjst@192.168.219.107>
2024-07-23 23:18:55 +0900

    240723

---
+++ semi/src/main/java/xyz/itwill/dto/QnaDTO.java
@@ -0,0 +1,102 @@
+package xyz.itwill.dto;
+
+/*
+ CREATE TABLE QNA (
+    QNA_NO NUMBER CONSTRAINT QNA_NO_PK PRIMARY KEY,
+    QNA_TITLE VARCHAR2(1000),
+    QNA_CONTENT VARCHAR2(2000),
+    QNA_USERS_NO NUMBER CONSTRAINT QNA_USERS_NO_FK REFERENCES USERS(USERS_NO),
+    QNA_STATUS NUMBER(1),
+    QNA_DATE DATE DEFAULT SYSDATE,
+    QNA_ANSWER VARCHAR2(2000),
+    QNA_PROD_NO NUMBER CONSTRAINT QNA_PROD_NO_FK REFERENCES PRODUCT(PROD_NO) 
+);
+
+CREATE SEQUENCE QNA_SEQ;
+
+DESC QNA;
+
+SELECT * FROM QNA;
+
+ */
+public class QnaDTO {
+	private int qnaNo;
+	private String qnaTitle;
+	private String qnaContent;
+	private int qnaUsersNo;
+	private int qnaStatus;
+	private String qnaDate;
+	private String qnaAnswer;
+	private int qnaProdNo;
+	
+	public QnaDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getQnaNo() {
+		return qnaNo;
+	}
+
+	public void setQnaNo(int qnaNo) {
+		this.qnaNo = qnaNo;
+	}
+
+	public String getQnaTitle() {
+		return qnaTitle;
+	}
+
+	public void setQnaTitle(String qnaTitle) {
+		this.qnaTitle = qnaTitle;
+	}
+
+	public String getQnaContent() {
+		return qnaContent;
+	}
+
+	public void setQnaContent(String qnaContent) {
+		this.qnaContent = qnaContent;
+	}
+
+	public int getQnaUsersNo() {
+		return qnaUsersNo;
+	}
+
+	public void setQnaUsersNo(int qnaUsersNo) {
+		this.qnaUsersNo = qnaUsersNo;
+	}
+
+	public int getQnaStatus() {
+		return qnaStatus;
+	}
+
+	public void setQnaStatus(int qnaStatus) {
+		this.qnaStatus = qnaStatus;
+	}
+
+	public String getQnaDate() {
+		return qnaDate;
+	}
+
+	public void setQnaDate(String qnaDate) {
+		this.qnaDate = qnaDate;
+	}
+
+	public String getQnaAnswer() {
+		return qnaAnswer;
+	}
+
+	public void setQnaAnswer(String qnaAnswer) {
+		this.qnaAnswer = qnaAnswer;
+	}
+
+	public int getQnaProdNo() {
+		return qnaProdNo;
+	}
+
+	public void setQnaProdNo(int qnaProdNo) {
+		this.qnaProdNo = qnaProdNo;
+	}
+	
+	
+	
+}	
