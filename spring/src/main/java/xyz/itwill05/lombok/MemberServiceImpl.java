e0fda7bb70eeb6c617e7faa5cdc0b242027d933e
itwill <itwill@DESKTOP-I0JRR21>
2024-08-23 16:48:39 +0900

    240823

--- spring/src/main/java/xyz/itwill05/lombok/MemberServiceImpl.java
+++ spring/src/main/java/xyz/itwill05/lombok/MemberServiceImpl.java
@@ -5,18 +5,43 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+//@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class MemberServiceImpl implements MemberService {
-	@Autowired
-	private MemberDAO memberDAO;
+	//@Autowired
+	//private MemberDAO memberDAO;
+	//private final MemberDAO memberDAO;
 	
+	@NonNull
+	private MemberDAO memberDAO;
+		
+	/*
 	public MemberServiceImpl() {
 		log.info("### MemberServiceImpl 클래스의 기본 생성자 호출 ###");
 	}
+	*/
 	
+	/*
+	//@Autowired
+	public MemberServiceImpl(MemberDAO memberDAO) {
+		log.info("### MemberServiceImpl 클래스의 매개변수가 작성된 생성자 호출 ###");
+		this.memberDAO=memberDAO;
+	}
+	*/
+	/*
+	@Autowired
+	public void setMemberDAO(MemberDAO memberDAO) {
+		this.memberDAO = memberDAO;
+	}
+	*/
+		
 	@Override
 	public void addMember(Member member) {
 		log.info("*** MemberDAOImpl 클래스의 addMember(Member member) 메소드 호출 ***");
