d9d2eb14b3c4ce301655bf8a0f280ef5a940818d
itwill <itwill@DESKTOP-I0JRR21>
2024-08-28 17:40:09 +0900

    240828

--- spring/src/main/java/xyz/itwill07/dao/DataSourceApp.java
+++ spring/src/main/java/xyz/itwill07/dao/DataSourceApp.java
@@ -11,7 +11,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 //DataSource 객체 : 다수의 Connection 객체를 미리 생성하여 저장하고 있는 객체 - DBCP(DataBase Connection Pool)
 // => Spring Bean Configuration File에서 DataSource 인터페이스를 상속받은 자식클래스를 Spring
 //Bean으로 등록해 스프링 컨테이너에 의해 관리되도록 설정
-//DataSource 인터페이스를 상속받은 자식클래스는 spring-jdbc 라이브러리를 빌처 처리하면 사용
+//DataSource 인터페이스를 상속받은 자식클래스는 spring-jdbc 라이브러리를 빌드 처리하면 사용
 //가능 - 메이븐 사용 : pom.xml
 // => spring-jdbc 라이브러리외에 Driver 클래스(ojdbc 라이브러리)를 제공하는 라이브러리도 
 //프로젝트에 빌드 처리
