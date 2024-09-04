7a0fc7dc7e81e41a65b5c22ae38cdd18c9b50c02
itwill <itwill@DESKTOP-I0JRR21>
2024-09-04 11:58:22 +0900

    240904

--- spring/src/main/java/xyz/itwill09/dto/Product.java
+++ spring/src/main/java/xyz/itwill09/dto/Product.java
@@ -8,11 +8,12 @@ import lombok.Data;
 
 @Data
 public class Product {
+	//@NotEmpty(message = "제품코드를 입력해 주세요.")
 	@NotEmpty
 	private String productCode;
 	@NotEmpty
 	private String productName;
-	@Min(value = 1)//필드에 저장될 최소값을 검증하기 위한 어노테이션
-	@Max(value = 100)//필드에 저장될 최대값을 검증하기 위한 어노테이션
+	@Min(value = 1)//필드에 저장될 최소값을 검증하기 위한 어너테이션
+	@Max(value = 100)//필드에 저장될 최대값을 검증하기 위한 어너테이션
 	private int productQuantity;
 }
