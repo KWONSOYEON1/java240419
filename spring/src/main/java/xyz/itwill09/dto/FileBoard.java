2a000df8353b37594500b1d1d0a728bdc193fba3
itwill <itwill@DESKTOP-I0JRR21>
2024-09-06 16:49:42 +0900

    240906

---
+++ spring/src/main/java/xyz/itwill09/dto/FileBoard.java
@@ -0,0 +1,11 @@
+package xyz.itwill09.dto;
+
+import lombok.Data;
+
+@Data
+public class FileBoard {
+	private int num;
+	private String writer;
+	private String subject;
+	private String filename;
+}
