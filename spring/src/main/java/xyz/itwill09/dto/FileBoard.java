93d2ff1d1ba93aff24d5990850fd78da94fb7d5b
itwill <itwill@DESKTOP-I0JRR21>
2024-09-06 17:49:33 +0900

    240906

--- spring/src/main/java/xyz/itwill09/dto/FileBoard.java
+++ spring/src/main/java/xyz/itwill09/dto/FileBoard.java
@@ -2,6 +2,9 @@ package xyz.itwill09.dto;
 
 import lombok.Data;
 
+//create table file_board(num number primary key, writer varchar2(20)
+//	, subject varchar2(100), filename varchar2(200));
+//create sequence file_board_seq;   
 @Data
 public class FileBoard {
 	private int num;
