ec4079e91b626c44989ef18219bcbe7a35abfe40
itwill <itwill@DESKTOP-I0JRR21>
2024-09-23 12:18:56 +0900

    240923

--- security/src/main/java/xyz/itwill/auth/CustomUserDetails.java
+++ security/src/main/java/xyz/itwill/auth/CustomUserDetails.java
@@ -13,8 +13,8 @@ import xyz.itwill.dto.SecurityAuth;
 import xyz.itwill.dto.SecurityUser;
 
 //인증된 사용자 정보와 권한 정보를 저장하기 위한 클래스
-// => 인증 성공 후 사용자 정보 및 권한 정보가 저장된 UserDetails 객체를 Spring Security로부타 
-//제공 받아 사용 가능하지만 사용자 정보 및 권한 정보를 원하는 형태로 제공받기 위해 클래스 작성 
+// => 인증 성공 후 인증 정보 및 권한 정보가 저장된 UserDetails 객체를 Spring Security로부터 제공  
+//받아 사용 가능하지만 사용자 정보 및 권한 정보를 원하는 형태로 제공받기 위해 클래스 작성 
 // => UserDetails 인터페이스를 상속받아 작성 - User 클래스를 상속받아 작성 가능
 @Data
 public class CustomUserDetails implements UserDetails {
@@ -28,7 +28,7 @@ public class CustomUserDetails implements UserDetails {
 	private String enabled;
 	
 	//인증된 사용자의 권한 정보가 저장될 필드 작성
-	private List<GrantedAuthority> securityAuthList;
+	private List<GrantedAuthority> secuthryAuthList;
 
 	//매개변수로 전달받은 SecurityUser 객체의 필드값을 CustomUserDetails 객체의 필드에 저장
 	public CustomUserDetails(SecurityUser user) {
@@ -38,17 +38,17 @@ public class CustomUserDetails implements UserDetails {
 		this.email=user.getEmail();
 		this.enabled=user.getEnabled();
 		
-		this.securityAuthList=new ArrayList<GrantedAuthority>();
+		this.secuthryAuthList=new ArrayList<GrantedAuthority>();
 		//검색된 사용자의 권한 정보를 GrantedAuthority 객체로 생성하여 List 객체의 요소값으로 저장
 		for(SecurityAuth auth : user.getSecurityAuthList()) {
-			securityAuthList.add(new SimpleGrantedAuthority(auth.getAuth()));
+			secuthryAuthList.add(new SimpleGrantedAuthority(auth.getAuth()));
 		}
 	}
 	
 	//인증된 사용자의 권한정보를 반환하는 메소드
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return securityAuthList;
+		return secuthryAuthList;
 	}
 
 	//인증된 사용자의 비밀번호를 반환하는 메소드
@@ -85,7 +85,7 @@ public class CustomUserDetails implements UserDetails {
 	}
 
 	//인증된 사용자의 활성화 상태를 반환하는 메소드
-	// => false : 사용자 활성화 상태, true : 사용자 비활성화 상태  
+	// => false : 사용자 비활성화 상태, true : 사용자 비활성화 상태  
 	@Override
 	public boolean isEnabled() {
 		if(enabled.equals("0")) {
---
+++ security/src/main/webapp/WEB-INF/views/form_login.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SECURITY</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+	<h1>로그인</h1>
+	<hr>
+	<form action="<c:url value="/user_login"/>" method="post" id="loginForm">
+	<table>
+		<tr>
+			<td>아이디</td>
+			<%-- 인증 처리 페이지에 아이디를 전달하는 name 속성값은 반드시 [username]으로 설정 --%>
+			<%-- <td><input type="text" name="username" id="userid"></td> --%>
+			<td><input type="text" name="userid" id="userid"></td>
+		</tr>
+		<tr>
+			<td>비밀번호</td>
+			<%-- 인증 처리 페이지에 비밀번호를 전달하는 name 속성값은 반드시 [password]으로 설정 --%>
+			<%-- <td><input type="password" name="password" id="passwd"></td> --%>
+			<td><input type="password" name="passwd" id="passwd"></td>
+		</tr>
+		<tr>
+			<td colspan="2"><button type="submit">로그인</button></td>
+		</tr>
+	</table>
+	<input type="hidden" name="${_csrf.parameterName }" value="${_csrf.token }">
+	</form>
+	<h3><a href="<c:url value="/"/>">메인페이지</a></h3>
+	
+	<script type="text/javascript">
+	$("#loginForm").submit(function() {
+		if($("#userid").val() == "") {
+			alert("아이디를 입력해 주세요.");
+			return false;
+		}		
+		
+		if($("#passwd").val() == "") {
+			alert("비밀번호를 입력해 주세요.");
+			return false;
+		}
+	});
+	</script>
+</body>
+</html>
+
+
+
+
