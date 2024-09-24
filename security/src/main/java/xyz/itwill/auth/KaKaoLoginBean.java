5b1cc4fe0a43ca44b5caf83c3f59628332bfc307
itwill <itwill@DESKTOP-I0JRR21>
2024-09-24 17:58:28 +0900

    240924

--- security/src/main/java/xyz/itwill/auth/KaKaoLoginBean.java
+++ security/src/main/java/xyz/itwill/auth/KaKaoLoginBean.java
@@ -18,8 +18,8 @@ import com.github.scribejava.core.oauth.OAuth20Service;
 //Kakao OpenAPI를 요청하기 위한 클래스
 @Component
 public class KaKaoLoginBean {
-	private final static String KAKAO_CLIENT_ID="9f127646d59dd6ac2e797e6b4083fa7b";
-	private final static String KAKAO_CLIENT_SECRET="WbBVtdcJnUhpJKnTPFnk9gkwZNyGJKnZ";
+	private final static String KAKAO_CLIENT_ID="3a7adab1e02df2c03e4ae296ea592619";
+	private final static String KAKAO_CLIENT_SECRET="cWKTAIxwCcuBffGl302NgTwCIh1Hydxe";
 	private final static String KAKAO_REDIRECT_URI="http://localhost:8000/kakao/callback";
 	private final static String SESSION_STATE="kakao_oauth_state";
 	//카카오 간편 로그인 후 사용자의 프로필 정보를 조회하기 위한 OpenAPI의 URL 주소
