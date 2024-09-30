e2fe8c4c06f79ea442a08878c3876fcb9b125902
itwill <itwill@DESKTOP-I0JRR21>
2024-09-30 13:11:23 +0900

    240930

--- boot/src/main/java/xyz/itwill/boot/controller/HomeController.java
+++ boot/src/main/java/xyz/itwill/boot/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/")
 	public String main(Model model) {
-		model.addAttribute("name", "ㅁ");
+		model.addAttribute("name", "홍길동");
 		return "index";
 	}
 }
